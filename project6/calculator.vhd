d2b027f5e612a3b3e95698afe94f86a3bc6643ef
QuRyu <Quentin.Liu.0415@gmail.com>
2017-10-29 21:53:23 -0400

    define and connect internal variables

--- project6/calculator.vhd
+++ project6/calculator.vhd
@@ -30,7 +30,40 @@ architecture rtl of calculator is
         );
     end component;
 
+    constant value_0  : std_logic_vecotr(7 downto 0) := "00000000";
+
+    signal stack_ptr  : unsigned(3 downto 0);         
+                            -- the pointer inside RAM to top locatiion
+    signal RAM_input  : std_logic_vector(7 downto 0); -- data to be stored inside RAM
+    signal RAM_output : std_logic_vector(7 downto 0); 
+                            -- data read from RAM pointed by stack_ptr
+    signal RAM_we     : std_logic;                    -- whether to write data
+    signal state      : std_logic_vector(3 downto 0); -- current state 
+    signal MBR        : std_logic_vector(7 downto 0); -- memory buffer 
+                            -- register, used to hold values moving to and from stack
+    signal TEMPR      : std_logic_vector(7 downto 0); -- register
+                            -- for holding values removed from stack
+
 begin 
+
+    value <= MBR;
+    stackview <= std_logic_vector(stack_ptr);
+    stateview <= state;
+    
+    process(reset, clk, b2, b3, b4) 
+    begin 
+        if reset = '0' then 
+            stack_ptr  <= "0000";
+            RAM_input  <= value_0;
+            RAM_output <= value_0;
+            RAM_we     <= '0';
+            state      <= "0000";
+        elsif rising_edge(clk) then 
+
+        end if;
+
+
+    end process;
 
 
 RAM : memram 
