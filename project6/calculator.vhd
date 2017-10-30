223012a36a8fee536ddf42a7ada4701c258658ba
QuRyu <Quentin.Liu.0415@gmail.com>
2017-10-29 22:33:31 -0400

    finish the main part

--- project6/calculator.vhd
+++ project6/calculator.vhd
@@ -59,7 +59,50 @@ begin
             RAM_we     <= '0';
             state      <= "0000";
         elsif rising_edge(clk) then 
-
+            case state is 
+                when "0000" => -- wait state
+                    if b2 = '0' then -- write input to MBR
+                        state <= "1111";
+                        MBR   <= data;
+                    elsif b3 = '0' then -- push MBR to stack
+                        state     <= "0001";
+                        RAM_we    <= '1';
+                        RAM_input <= MBR;
+                    elsif b4 = '0' then -- execute operation
+                        state <= "0010";
+                        if not(stack_ptr) = "0000" then 
+                            stack_ptr <= stack_ptr - 1;
+                        end if;
+                    end if;
+                when "0001" => 
+                    RAM_we    <= '0';
+                    state     <= "1111";
+                    stack_ptr <= stack_ptr + 1;
+                when "0010" => -- wait for retrieving value
+                    state <= "0011";
+                when "0011" => -- wait for retrieving value
+                    state <= "0100";
+                when "0100" => -- get value!
+                    TEMPR <= RAM_output;
+                when "0101" => -- execute arithmetic operations
+                    case op is 
+                        when "00" => -- add 
+                            MBR <=
+                           std_logic_vector(unsigned(MBR) + unsigned(TEMPR));
+                        when "01" => -- minus 
+                            MBR <= 
+                           std_logic_vector(unsigned(MBR) - unsigned(TEMPR));
+                        when "10" => -- times
+                            MBR <= 
+                           std_logic_vector(unsigned(MBR(3 downto 0)) *
+                                            unsigned(TEMPR(3 downto 0)));
+                        when "11" => -- divide
+                            MBR <=  -- TODO finish the division 
+                           std_logic_vector(divide(
+                    end case;
+                when "1111" =>
+                    state <= "0000";
+            end case;
         end if;
 
 
