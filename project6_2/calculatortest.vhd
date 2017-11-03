ee7430b8c79a081146ae0c7c5bacddd15c8ef583
QuRyu <Quentin_L_0415@outlook.com>
2017-11-03 15:37:07 -0400

    test newly added operation

--- project6_2/calculatortest.vhd
+++ project6_2/calculatortest.vhd
@@ -27,7 +27,7 @@ architecture test of calculatortest is
          b2:    in std_logic; -- switch values to mbr
          b3:    in std_logic; -- push mbr -> stack
          b4:    in std_logic; -- pop stack -> mbr
-         op:    in std_logic_vector(1 downto 0); -- operation 
+         op:    in std_logic_vector(2 downto 0); -- operation 
          data:  in std_logic_vector(7 downto 0);
          digit0: out std_logic_vector(6 downto 0);
          digit1: out std_logic_vector(6 downto 0);
@@ -49,7 +49,7 @@ architecture test of calculatortest is
   signal digit1 : std_logic_vector(6 downto 0);
   
   -- operation
-  signal op : std_logic_vector(1 downto 0);
+  signal op : std_logic_vector(2 downto 0);
 
 begin
 
@@ -74,22 +74,20 @@ begin
   -- We're going to push 1, 2, and 3 onto the stack at time 50, 100, and 150,
   -- then put a "0000" into the MBR register before popping the three values
   -- off the stack
-  data <= "00000101";
+  data <= "00000010", "00000101" after 90 ns;
 
   -- put data values into the MBR at 50, 100, 150
-  b2 <= '1', '0' after 55 ns, '1' after 70 ns;
+  b2 <= '1', '0' after 55 ns, '1' after 70 ns, '0' after 100 ns, '1' after 120 ns;
 
   -- push three values (1, 2, 3) onto the stack using b3 at time 70, 120, 170
-  b3 <= '1', '0' after 75 ns, '1' after 80 ns, '0' after 100 ns, '1'
-after 110 ns;
+  b3 <= '1', '0' after 75 ns, '1' after 80 ns;
 
   -- pop three values off the sack using b4 at times 220, 280, 340, should get
   -- 3, 2, 1 in that order
-  b4 <= '1', '0' after 150 ns, '1' after 160 ns, '0' after 250 ns, '1'
-after 260 ns;
+  b4 <= '1', '0' after 150 ns, '1' after 160 ns;
 
   -- operation
-  op <= "11", "00" after 200 ns;
+  op <= "100";
 
   -- port map the circuit
   L0: calculator port map(clk, reset, b2, b3, b4, op, data, digit0, digit1, stackview, stateview);
