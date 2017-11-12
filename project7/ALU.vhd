e236737852cf41363298215ef37d459cf1497d93
QuRyu <Quentin_L_0415@outlook.com>
2017-11-12 16:55:46 -0500

    fix ALU bugs

--- project7/ALU.vhd
+++ project7/ALU.vhd
@@ -27,16 +27,17 @@ entity alu is
     srcA : in  unsigned(15 downto 0);         -- input A
     srcB : in  unsigned(15 downto 0);         -- input B
     op   : in  std_logic_vector(2 downto 0);  -- operation
-    cr   : out std_logic_vector(3 downto 0);  -- condition outputs
-    dest : out unsigned(15 downto 0));        -- output value
+    cr   : out std_logic_vector(3 downto 0) := "0000";  -- condition outputs
+    dest : out unsigned(15 downto 0)        -- output value
+  );
 
-end alu;
+end entity;
 
 architecture test of alu is
 
   -- The signal tdest is an intermediate signal to hold the result and
   -- catch the carry bit in location 16.
-  signal tdest : unsigned(16 downto 0);  
+  signal tdest : unsigned(16 downto 0) := "00000000000000000";  
   
   -- Note that you should always put the carry bit into index 16, even if the
   -- carry is shifted out the right side of the number (into position -1) in
@@ -44,74 +45,51 @@ architecture test of alu is
   -- condition flag in the case of a carry out.
 
 begin  -- test
+
+
   process (srcA, srcB, op)
   begin  -- process
-    cr <= "0000"; -- initialize cr 
     case op is
-      when "000" =>        -- addition     dest = srcA + srcB
-          dest <= std_logic_vector(srcA + srcB);
-          if (dest(15) xor srcA(15)) and (dest(15) xor srcB(15)) then 
-              cr(1) <= '1'; -- overflow
-          end if;
-          if dest(15) = '0' then 
-              cr(3) <= '1'; -- carry 
-          end if;
-      when "001" =>        -- subtraction  dest = srcA - srcB
-          dest <= srcA - srcB;
-          if (dest(15) xor srcA(15)) and (dest(15) xor srcB(15)) then 
-              cr(1) <= '1'; -- overflow 
-          end if;
-          if dest(15) = '1' then 
-              cr(3) <= '0'; -- carry
-          end if;
-      when "010" =>        -- and          dest = srcA and srcB
-          dest <= srcA and srcB;
-      when "011" =>        -- or           dest = srcA or srcB
-          dest <= srcA or srcB;
-      when "100" =>        -- xor          dest = srcA xor srcB
-          dest <= srcA xor srcB;
-      when "101" =>        -- shift        dest = src shifted left arithmetic by one if srcB(0) is 0, otherwise right
+      when "000" =>        -- addition     tdest = srcA + srcB
+          tdest <= (srcA(15) & srcA) + (srcB(15) & srcB);
+      when "001" =>        -- subtraction  tdest = srcA - srcB
+          tdest <= (srcA(15) & srcA) - (srcB(15) & srcB);
+      when "010" =>        -- and          tdest = srcA and srcB
+          tdest <= '0' & (srcA and srcB);
+      when "011" =>        -- or           tdest = srcA or srcB
+          tdest <= '0' & (srcA or srcB);
+      when "100" =>        -- xor          tdest = srcA xor srcB
+          tdest <= '0' & (srcA xor srcB);
+      when "101" =>        -- shift        tdest = src shifted left arithmetic by one if internalB(0) is 0, otherwise right
           if srcB = 0 then 
-              if srcA(15) = '1' then 
-                  cr(3) <= '1';
-              else 
-                  cr(3) <= '0'; 
-              end if;
-              dest <= shift_left(srcA, 1);
+              tdest <= '0' & shift_left(srcA, 1);
           else 
-              if srcA(0) = '1' then 
-                  cr(3) <= '1';
-              else 
-                  cr(3) <= '0'; 
-              end if;
-              dest <= shift_right(srcA, 1);
+              tdest <= '0' & shift_right(srcA, 1);
           end if;
-      when "110" =>        -- rotate       dest = src rotated left by one if srcB(0) is 0, otherwise right
+      when "110" =>        -- rotate       tdest = src rotated left by one if internalB(0) is 0, otherwise right
           if srcB = 0 then 
-              dest <= rotate_left(srcA, 1);
+              tdest <= '0' & rotate_left(srcA, 1);
           else 
-              dest <= rotate_right(srcA, 1);
-      when "111" =>        -- pass         dest = srcA
-          dest <= srcA;
+              tdest <= '0' & rotate_right(srcA, 1);
+          end if;
+      when "111" =>        -- pass         tdest = srcA
+          tdest <= '0' & srcA;
       when others =>
         null;
     end case;
-
-    if dest = 0 then 
-        cr(0) <= '1';
-    else 
-        cr(0) <= '0'; -- restore the bit if set previously
-    end if;
-
-    if dest(15) = '1' then 
-        cr(2) <= '1'; 
-    else 
-        cr(2) <= '0';
-    end if;
   end process;
 
   -- connect the low 16 bits of tdest to dest here
+  dest <= tdest(15 downto 0);
 
   -- set the four CR output bits here
+  cr(0) <= '1' when tdest = 0 else 
+           '0';
+  cr(1) <= '1' when (srcA(15) xor tdest(15)) = '1' and (srcB(15) xor tdest(15)) = '1' else 
+           '0';
+  cr(2) <= '1' when tdest(15) = '1' else 
+           '0';
+  cr(3) <= '1' when tdest(16) = '1' else 
+           '0';
 
 end test;
