1f2f1689e23d453aa35caeb599aea52a51edf320
QuRyu <Quentin.Liu.0415@gmail.com>
2017-11-15 06:35:12 -0500

    fix a bug related to the template

--- project7/assembler/assembler.cpp
+++ project7/assembler/assembler.cpp
@@ -11,15 +11,16 @@
 
 using namespace std;
 
-map<string, int> tokens = {{"Load", 0000}, {"Store", 0001}, 
-    {"Jump", 0010}, {"Branch", 001100}, {"Call", 001101}, 
-    {"Return", 001110}, {"Exit", 001111}, {"Push", 0100}, 
-    {"Pop", 0101}, {"Output", 0110}, {"Input", 0111}, 
-    {"Add", 1000}, {"Subtract", 1001}, {"And", 1010}, 
-    {"Or", 1011}, {"ExOr", 1100}, {"Shift", 1101}, 
-    {"Rotate", 1110}, {"Move", 1111}, 
-    {"RA", 000}, {"RB", 001}, {"RC", 010}, {"RD", 011},
-    {"RE", 100}, {"SP", 101}, {"PC", 110}, {"CR", 111}};
+map<string, string> tokens = {{"Load", "0000"}, {"Store", "0001"}, 
+    {"Jump", "0010"}, {"Branch", "001100"}, {"Call", "001101"}, 
+    {"Return", "001110"}, {"Exit", "001111"}, {"Push","0100"}, 
+    {"Pop", "0101"}, {"Output", "0110"}, {"Input", "0111"}, 
+    {"Add", "1000"}, {"Subtract", "1001"}, {"And", "1010"}, 
+    {"Or", "1011"}, {"ExOr", "1100"}, {"Shift", "1101"}, 
+    {"Rotate", "1110"}, {"Move", "1111"}, 
+    {"Zero", "00"}, {"Overflow", "01"}, {"Neg", "10"}, {"Carry", "11"},
+    {"RA", "000"}, {"RB", "001"}, {"RC", "010"}, {"RD", "011"},
+    {"RE", "100"}, {"SP", "101"}, {"PC", "110"}, {"CR", "111"}};
 
 
 int main(int argc, char **argv) {
@@ -34,9 +35,8 @@ int main(int argc, char **argv) {
         istringstream iss(s);
         stringstream oss;
         while (iss >> token) {
-            if (tokens.find(token) != tokens.end()) {
+            if (tokens.find(token) != tokens.end()) 
                 oss << tokens[token];
-            }
             else 
                 oss << token;
         }
